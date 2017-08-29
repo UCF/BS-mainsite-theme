c0ab05988dce434242f7ab9718eb5227b9628b60
Mike Setzer <michaelsetzerucf@gmail.com>
2017-08-29 14:15:41 -0400

    Contact Warning
    - Added a notification to stop students from submitting work orders accidentally because apparently it's that hard to read.

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -14,6 +14,7 @@ Template Name: Contact
 
 				<div class="container">
 
+					<div class="notice"><strong>Please use the dining membership form in the tabs below to request a cancellation.</strong></div>
 					<div class="inner-wrap-container">
 
 						<div class="top-wrap formatted pull-right">
