1c21934d30caad95b3eae6f4756213e9b1d5bc9c
Mike Setzer <michaelsetzerucf@gmail.com>
2019-06-26 14:07:28 -0400

    Retail Template Added

---
+++ template-retail.php
@@ -0,0 +1,40 @@
+<?php /*
+Template Name: Retail
+*/ ?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+
+		<main id="content" class="Retail no-padding-top">
+
+			<section class="slider-section has-padding-2">
+
+				<div class="container">
+
+					<div class="inner-wrap-container">
+						<div class="notice"><strong>Please use the dining membership form in the tabs below to request a cancellation.</strong></div>
+
+						<div class="top-wrap formatted pull-right">
+
+							<div class="pagination no-margin-top">
+								<a href="#" class="left-page pull-left"></a>
+								<a href="#" class="right-page pull-left"></a>
+								<div class="clear"></div>
+							</div>
+
+						</div>
+
+						<div class="clear"></div>
+
+					</div>
+
+				</div>
+
+			</section>
+
+		</main>
+
+	</div>
+
+<?php get_footer(); ?>
