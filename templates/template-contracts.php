123c091c0b292104b56edc9ba7990e41d0d4c22c
Mike Setzer <michaelsetzerucf@gmail.com>
2021-05-26 13:45:09 -0400

    Update template-contracts.php

--- templates/template-contracts.php
+++ templates/template-contracts.php
@@ -16,41 +16,36 @@ Template Name: Contracts
 
                 <div class="inner-wrap-container">
 
-                    <div class="top-wrap formatted">
+                    <div class="content-wrap active">
 
                         <?php if( have_rows('contracts_repeater') ): ?>
 
+                            <div class="contracts-repeater inner-wrap-container">
 
-                            <div class="content-wrap active">
-                                <div class="contracts-repeater inner-wrap-container">
+                                <?php while( have_rows('contracts_repeater') ): the_row();
 
-                                    <?php while( have_rows('contracts_repeater') ): the_row();
-
-                                        $image = get_sub_field('contract_image');
-                                        ?>
-                                        <div class="repeater-item imager-container-formatted">
-                                            <div class="image-side pull-left">
-                                                <?php echo wp_get_attachment_image( $image, 'full' ); ?>
-                                            </div>
-                                            <div class="text-side pull-right">
-                                                <h2><?php the_sub_field('contract_title'); ?></h2>
-                                                <p class="contract-tagline"><?php the_sub_field('contract_tagline'); ?></p>
-                                                <p class="contract-body"><?php the_sub_field('contract_body_text'); ?></p>
-                                                <p class="contract-links"><?php the_sub_field('contract_links'); ?></p>
-                                            </div>
+                                    $image = get_sub_field('contract_image');
+                                    ?>
+                                    <div class="repeater-item imager-container-formatted">
+                                        <div class="image-side pull-left">
+                                            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
                                         </div>
 
-                                    <?php endwhile; ?>
+                                        <div class="text-side pull-right">
+                                            <h2><?php the_sub_field('contract_title'); ?></h2>
+                                            <p class="contract-tagline"><?php the_sub_field('contract_tagline'); ?></p>
+                                            <p class="contract-body"><?php the_sub_field('contract_body_text'); ?></p>
+                                            <p class="contract-links"><?php the_sub_field('contract_links'); ?></p>
+                                        </div>
+                                    </div>
 
-                                </div>
+                                <?php endwhile; ?>
+
                             </div>
 
                         <?php endif; ?>
-
-
+                        
                     </div>
-
-
 
                 </div>
 
