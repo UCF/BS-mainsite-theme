004e8e705e5a71eb994f190f8ed23e0d296c20c3
Mike Setzer <michaelsetzerucf@gmail.com>
2022-07-14 10:42:13 -0400

    Added new advertising template

---
+++ template-parts/advertising-forms/app-ad.php
@@ -0,0 +1,19 @@
+<div class="content-wrap active" data-wrap="slide-1">
+	
+	<?php if ( get_field( 'contact_form_description' ) ) { ?><!--TO DO ADD FIELD-->
+
+		<div class="info-block formatted2">
+			
+			<div class="top-wrap">
+				<?php the_field( 'contact_form_description' ); ?><!--TO DO - ADD FIELDS-->
+			</div>	
+
+		</div>
+
+	<?php } ?>
+
+	<div class="form-wrap">
+		<?php gravity_form( 1, FALSE, FALSE, FALSE, NULL, TRUE); ?><!--TO DO - Update Form -->
+	</div>
+
+</div>
---
+++ templates/template-advertising.php
@@ -0,0 +1,55 @@
+<?php /*
+Template Name: Advertising
+*/ ?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+
+		<main id="content" class="home">
+
+			<section class="slider-section">
+
+				<div class="container">
+
+					<div class="inner-wrap-container">
+
+						<div class="pagination catering slider-line-pagers">
+							<a href="#" class="left-page pull-left"></a>
+							<a href="#" class="right-page pull-left"></a>
+							<div class="clear"></div>
+						</div>
+
+						<ul class="slider-line full-border">
+
+							<li class="pull-left">
+								<a href="#" data-wrap="slide-0" data-slide="0">Shuttle Ad</a>
+							</li>
+
+							<li class="pull-left">
+								<a href="#" data-wrap="slide-1" data-slide="1">UCF App Ad</a>
+							</li>
+
+						</ul>
+
+						<div class="clear"></div>
+
+						<div id="slideCount" data-total-slides="2"></div>
+
+                        <!--TO DO -->
+						<?php
+							get_template_part( 'template-parts/advertising-forms/app-ad' ); 
+							get_template_part( 'template-parts/advertising-forms/shuttle-ad' );
+						?>
+
+					</div>
+
+				</div>
+
+			</section>
+
+		</main>
+
+	</div>
+
+<?php get_footer(); ?>
